f176ac95dbec5efde62cd340b8cdedb61374b3fe
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-28 11:17:20 +0530

    Add or update file: UserController.java

---
+++ src/main/java/com/app/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.app.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    // Placeholder for user-related endpoints
+}
