0a862442726b3bc0f807aaa5a72c7f75fc077522
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-28 11:17:38 +0530

    Add or update file: RewardsController.java

---
+++ src/main/java/com/app/controller/RewardsController.java
@@ -0,0 +1,10 @@
+package com.app.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/rewards")
+public class RewardsController {
+    // Placeholder for rewards-related endpoints
+}
