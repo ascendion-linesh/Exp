1ec5013220e05167be7b5bfd8d6c57547cb51ffd
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-29 11:00:56 +0530

    Add or update file: UserRegistrationRequest.java

---
+++ user-service/src/main/java/com/example/userservice/dto/UserRegistrationRequest.java
@@ -0,0 +1,23 @@
+package com.example.userservice.dto;
+
+import lombok.Data;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+public class UserRegistrationRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+}
