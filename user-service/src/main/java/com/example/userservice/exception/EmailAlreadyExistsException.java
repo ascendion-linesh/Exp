580bc6eda82dd10beb40dd6579823c24285eda10
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-29 11:01:15 +0530

    Add or update file: EmailAlreadyExistsException.java

---
+++ user-service/src/main/java/com/example/userservice/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.userservice.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
