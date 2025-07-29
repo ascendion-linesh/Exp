a71e8ea394fd12fb763bc87b0d516d11f7455011
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-29 10:38:51 +0530

    Add or update file: UserServiceClient.java

--- orders-service/src/main/java/com/example/orderservice/feign/UserServiceClient.java
+++ orders-service/src/main/java/com/example/orderservice/feign/UserServiceClient.java
@@ -1,12 +1,12 @@
 package com.example.orderservice.feign;
 
-import com.example.orderservice.dto.UserResponseDto;
+import com.example.orderservice.feign.dto.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "user-service", url = "${user-service.url}")
 public interface UserServiceClient {
-    @GetMapping("/users/{id}")
-    UserResponseDto getUserById(@PathVariable("id") Long id);
+    @GetMapping("/api/users/{id}")
+    UserDto getUserById(@PathVariable("id") Long id);
 }
