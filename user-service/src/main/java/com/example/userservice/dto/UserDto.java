a5a31a0d436dea360bc0c41248f31c1670670ece
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-30 13:31:28 +0530

    Add or update file: UserDto.java

---
+++ user-service/src/main/java/com/example/userservice/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.example.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private int totalOrders;
+    private double totalSpent;
+}
