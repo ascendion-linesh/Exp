fb3ab3310bf67e826aad0fa858a53ac279e992be
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-30 13:37:55 +0530

    Add or update file: LoyaltyActionDto.java

---
+++ rewards-service/src/main/java/com/example/rewardservice/dto/LoyaltyActionDto.java
@@ -0,0 +1,13 @@
+package com.example.rewardservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoyaltyActionDto {
+    private String actionType;
+    private int points;
+}
