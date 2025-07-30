f3b26efffb542d545da04658bb20cc4a5e676c1f
ascendion-linesh <linesh.mishra@ascendion.com>
2025-07-30 13:28:55 +0530

    Delete user-service directory

--- user-service/src/main/java/com/example/userservice/exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.userservice.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    public EmailAlreadyExistsException(String message) {
-        super(message);
-    }
-}
